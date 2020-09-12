c66b7adc0782401719df17a9d0f86fe876660b04
Myeong-Seob <69719857+Myeong-Seob@users.noreply.github.com>
2020-09-12 19:39:28 +0900

    Make Random Number between 1 to 100

---
+++ main.js
@@ -0,0 +1,30 @@
+const input__number = document.querySelector(".input"),
+  userNum = input__number.querySelector("input"),
+  submit = document.querySelector(".submit"),
+  list = document.querySelector(".submit__list"),
+  count__number = document.querySelector(".count"),
+  win = document.querySelector(".win"),
+  lose = document.querySelector(".lose"),
+  didNum = document.querySelector(".didNum"),
+  bigger = document.querySelector(".bigger"),
+  lower = document.querySelector(".lower"),
+  body = document.querySelector("body");
+reset = document.querySelector(".reset");
+
+const SHOWING_CL = "showing";
+const SHOW = "show";
+const DOWN = "down";
+const HINT = "hint";
+const list_num = [];
+
+function makeRndNum() {
+  const randomNum = Math.floor(Math.random() * 100) + 1;
+  return randomNum;
+}
+
+function init() {
+  const randomNum = makeRndNum();
+  console.log(randomNum);
+}
+
+init();
